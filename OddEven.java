daba387a5079848c3d8f2f00c899c3751c1eb4bb
Amaran27 <amarancse@gmail.com>
2017-05-21 03:34:40 +0000

    Create OddEven.java

---
+++ OddEven.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.Scanner;
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc=new Scanner(System.in);
+		int input=sc.nextInt();
+		if(input%2==0)
+		System.out.println("Even");
+		else
+		System.out.println("Odd");
+	}
+}
